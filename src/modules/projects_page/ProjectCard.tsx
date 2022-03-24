9b1c27e7969766e7dde316ea37b02e1c15a0f4a1
kyle65463 <kyle65463@gmail.com>
2022-03-24 13:44:19 +0800

    Refactor project content

--- src/modules/projects_page/ProjectCard.tsx
+++ src/modules/projects_page/ProjectCard.tsx
@@ -12,11 +12,11 @@ export default function ProjectCard({ title, content, image, link }: ProjectCard
 	return (
 		<div className='card w-80 bg-base-100 shadow-xl'>
 			<figure>
-				<Image src={image} width={1440 / 4.5} height={789 / 4.5} layout='fixed' />
+				<Image src={image} width={1440 / 4.4} height={789 / 4.4} layout='fixed' />
 			</figure>
 			<div className='card-body'>
 				<h2 className='card-title'>{title}</h2>
-				{content}
+				<div className='text-content-light'>{content}</div>
 			</div>
 		</div>
 	);
--- src/modules/projects_page/ProjectsPage.tsx
+++ src/modules/projects_page/ProjectsPage.tsx
@@ -5,17 +5,23 @@ import ProjectSection from "./ProjectSection";
 export default function ProjectPages() {
 	const projects = [
 		{
-			title: "背日文神器",
+			title: "Japanese Memorizer",
 			content: (
 				<>
 					<p>
-						「背日文神器」是學習日文單字的最佳工具，使用單字卡的概念，配合多種類型的題目，幫助迅速記住單字，輕鬆通過日文檢定！並且，App
-						中提供了數千的免費可下載單字，涵蓋 N1~N5 不同等級，與各種日常主題。
+						A mobile app to help memorize Japanese vocabulary with flashcards and the spaced repetition
+						mechanism.
 					</p>
-					<br />
-					<p>
-						「背日文神器」使用 Flutter/Dart 進行開發，並以 Firebase 為後端，在 iOS/Android
-						雙平台上都有上架。目前已有數千下載次數，每月活躍使用者也超過一千人。
+					<p className='pt-3'>
+						The app was published on both the App Store and Google Play Store, having{" "}
+						<span className='text-primary'>13K+ downloads</span> ,{" "}
+						<span className='text-primary'>4.5+ average star rates</span>, and{" "}
+						<span className='text-primary'>2.5K+ monthly active users</span>.
+					</p>
+					<p className='pt-3'>
+						The app was designed and developed by myself from scratch using the{" "}
+						<span className='text-primary'>Flutter</span> framework. And the web version with React as
+						frontend and Google Cloud Function as backend is under development.
 					</p>
 				</>
 			),
@@ -24,81 +30,44 @@ export default function ProjectPages() {
 				"https://apps.apple.com/us/app/%E8%83%8C%E6%97%A5%E6%96%87%E7%A5%9E%E5%99%A8-%E6%97%A5%E6%AA%A2%E5%BF%85%E5%82%99%E5%96%AE%E5%AD%97%E6%95%91%E6%98%9F/id1550577799",
 			playstore: "https://play.google.com/store/apps/details?id=com.kyle65463.japanese_memorizer",
 		},
-		{
-			title: "Cuby - 魔術方塊計時器",
-			content: (
-				<>
-					<p>
-						「Cuby」是一個魔術方塊計時器
-						App，以「簡潔」作為核心的設計理念。除了提供精美的統計圖表，還有許多方便的快捷手勢可以快速刪除紀錄或打亂新的方塊等功能。
-					</p>
-					<br />
-					<p>
-						這個 App 的靈感來自於 Twisty Timer，並且還在開發中，一樣使用了 Flutter/Dart
-						作為開發框架，目前只支援生成 3x3x3 方塊的打亂步驟。
-					</p>
-				</>
-			),
-			image: "/projects/cube-timer.png",
-			github: "https://github.com/kyle65463/cube-timer",
-			appstore: "https://apps.apple.com/us/app/cuby-intuitive-cube-timer/id1583455340",
-			playstore: "https://play.google.com/store/apps/details?id=com.kyle65463.cubetimer",
-		},
-		{
-			title: "皇輿爭霸 Dominion",
-			content: (
-				<>
-					<p>
-						在台大基礎物件導向程式設計 (FOOP)
-						課程的期末專題作品。皇輿爭霸是一款經典的卡牌桌遊，我們依照它的規則復刻出了電腦版，並且可以與其他玩家線上進行遊戲。皇輿爭霸是一款構造牌組的遊戲，藉由購買卡片擴展牌組，組成不同的
-						combo，獲得分數而贏得勝利。
-					</p>
-					<br />
-					<p>
-						在開發上我們主要程式語言使用 Java 進行物件導向的設計，並搭配 JavaFX 作為 GUI 框架。連線方面使用
-						Java 內建的 web socket，原始碼總行數高達六千多行。
-					</p>
-					<br />
-					<p>組員： kyle65463、smile04455660、misterSu514</p>
-					<p>
-						Demo影片：{" "}
-						<a className='text-primary' href='https://www.youtube.com/watch?v=gHtup-ZRhrU'>
-							https://www.youtube.com/watch?v=gHtup-ZRhrU
-						</a>
-					</p>
-					<p>
-						正版遊戲網站連結：{" "}
-						<a className='text-primary' href='https://dominion.games/'>
-							https://dominion.games/
-						</a>
-					</p>
-				</>
-			),
-			image: "/projects/dominion.png",
-			github: "https://github.com/kyle65463/dominion",
-		},
 	];
 
 	const projects2 = [
 		{
-			title: "JS Tetris",
+			title: "Cuby",
 			link: "https://kyle65463.github.io/JS-Tetris/",
-			content: "復刻經典遊戲「Tetris」俄羅斯方塊，使用純 Javascript + HTML canvas 開發",
-			image: "/projects/js-tetris.png",
+			content:
+				"A timer for rubik's cube inspired by Twisty Timer. Currently available on Play Store and App Store.",
+			image: "/projects/cuby.png",
 			github: "https://github.com/kyle65463/js-tetris",
 		},
 		{
 			title: "GitHub Repo Viewer",
 			link: "https://kyle65463.github.io/JS-Tetris/",
-			content: "復刻經典遊戲「Tetris」俄羅斯方塊，使用純 Javascript + HTML canvas 開發",
+			content: "A simple application that can list GitHub repositories of specific users.",
 			image: "/projects/github-viewer.png",
 			github: "https://github.com/kyle65463/js-tetris",
 		},
 		{
 			title: "Intersection Managment",
 			link: "https://kyle65463.github.io/JS-Tetris/",
-			content: "復刻經典遊戲「Tetris」俄羅斯方塊，使用純 Javascript + HTML canvas 開發",
+			content:
+				"A implementation and visualization of different algorithms solving the intersection management problem.",
 			image: "/projects/intersection.png",
+			github: "https://github.com/kyle65463/js-tetris",
+		},
+		{
+			title: "Dominion",
+			link: "https://kyle65463.github.io/JS-Tetris/",
+			content: 'A replica of the famous board game "Dominion" using JavaFX and sockets.',
+			image: "/projects/dominion.png",
+			github: "https://github.com/kyle65463/js-tetris",
+		},
+		{
+			title: "JS Tetris",
+			link: "https://kyle65463.github.io/JS-Tetris/",
+			content: "A replica of a classic game, Tetris, developed with pure Javascript + HTML canvas.",
+			image: "/projects/js-tetris.png",
 			github: "https://github.com/kyle65463/js-tetris",
 		},
 	];
@@ -107,9 +76,9 @@ export default function ProjectPages() {
 			{projects.map((project, index) => (
 				<ProjectSection {...project} key={index} transparent={index % 2 == 0} />
 			))}
-			<section className='page-px pt-20 pb-24 bg-base-300'>
-				<h2 className='text-xl md:text-4xl font-bold pb-5'>其他專案</h2>
-				<div className='grid grid-cols-3 pt-6 '>
+			<section className='page-px pt-20 pb-40 bg-base-300'>
+				<h2 className='font-roboto text-3xl font-medium pb-6'>Others</h2>
+				<div className='grid grid-cols-3 pt-6 gap-y-8'>
 					{projects2.map((project, index) => (
 						<ProjectCard {...project} key={index} />
 					))}
